cb99d0edfe4ce35df8547955c93701cac682c435
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-08 10:30:33 -0700

    VAriables if,else,while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		putchar(%x)
+	}
+	{
+		putchar(i)
+	}
+	{
+		putchar('\n)
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - possible combinatin of single digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int mai(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar("i, ")
+	}
+	{
+		putchar(%i);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
