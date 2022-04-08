2c2018000d8837e4d2b932c416397f3a0d4a147a
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-08 11:39:01 -0700

    VAriables if,else,while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar("%d", i);
+		putchar(i);
 	}
 	{
-		putchar("\n");
+		putchar('\n');
 	}
 	return (0);
 }
