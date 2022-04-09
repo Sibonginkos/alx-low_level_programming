ba250727c7d76b01c0009ff65aef48e836cfdfa6
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-09 03:43:17 -0700

    Variables; if,else, while

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print combination of three digits
+ * Results: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i,p,q;
+	for (i = '0'; i < '9'; i++)
+	{
+		for (p = i + 1; p <= '9'; p++)
+	{
+		for (q = p + 1; q <= '9'; q++)
+	{
+		if ((p != i) != q)	
+		{
+			putchar(i);
+			putchar(p);
+			putchar(q);
+
+			if (i == '7' && p == '8')
+				continue;
+			putchar(',');
+			putchar(' ');
+		}		
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
