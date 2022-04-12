92a646118c3a5c1222ea67f5d1870d5e6a91c79c
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-12 04:18:35 -0700

    Lowe cases- alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Print alphabet
  * Return: Always 0 (succes)
  */
 
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
