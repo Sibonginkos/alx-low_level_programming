f0ac36d0aaf1fd5e214e83107cc80004e9cb249b
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-12 11:08:40 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,9 +13,9 @@ int _isalpha(int c)
 	char lower, upper;
 	int isletter = 0;
 
-	for (lower = 'a'; lower = 'z'; lower++)
+	for (lower = 'a'; lower <= 'z'; lower++)
 	{
-		for (upper = 'A'; upper = 'Z'; upper++)
+		for (upper = 'A'; upper <= 'Z'; upper++)
 		{
 			if (c == lower || c == upper)
 				isletter = 1;
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * int print_sign(int n); - prints sign of a number
+ * int print_sign(int n); - Prints sign of a number
  * @n: An input number
  * Return: 1 if positive, 0 if zero or -1 if negative
  */
@@ -10,6 +10,7 @@
 int print_sign(int n)
 {
 	int value;
+
 	if (n > 0)
 	{
 		value = 1;
