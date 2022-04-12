898ea9522a48920deb7298bfeb9b00b78e5544e3
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-12 11:49:01 -0700

    Integer absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * int _abs - Print absolute values of intergers
+ * Return: Absolute value of r
+ */
+
+int _abs(int)
+{
+	int r;
+
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
