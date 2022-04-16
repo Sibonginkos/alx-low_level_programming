f699664e1fb7184adf0dce4da6d4c1208ca0409d
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-16 09:10:17 -0700

    98 Battery Street

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n - A input integer
+ * Return: Nothing
+ */
+
+int print_to_98(int n)
+{
+	if (n <= 98; n++)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
