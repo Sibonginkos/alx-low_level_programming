82e0de13171861e8e52d81f3c9687579931ab226
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-18 03:54:33 -0700

    Uppercase character

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _putchar(char c);
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,4 +11,3 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 int main(void);
-int _putchar(char);
