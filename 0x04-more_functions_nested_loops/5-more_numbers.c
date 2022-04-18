af9a695cce813d807844266d55c4903732b62268
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-18 02:12:46 -0700

    10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,7 @@ void more_numbers(void)
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
-		{
-			if (i > 9)
-				_putchar(i / 10 + '0');
-
-			_putchat(i % 10 + '0');
-		}
+			_putchar(i);
 		_putchar('\n');
 	}
 }
