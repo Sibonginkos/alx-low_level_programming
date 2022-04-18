4e3869a21d794c9927aa2b76ab5ec65c992c3951
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-18 08:26:07 -0700

    Largest prime factor

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,15 @@
  * Results: Nothing
  */
 
-void print_mumber(int n)
+void print_number(int n)
 {
 	if (n == 0)
 		_putchar('0');
 	else if (n < 0)
 	{
 		_putchar('-');
-		print_integer(n * -1);
+		print_number(n * -1);
 	}
 	else
-		print_integer(n);
+		print_number(n);
 }
