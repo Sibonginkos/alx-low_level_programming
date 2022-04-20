f89777d9c92cfa20d7b62fe4b4cadce649f480d8
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-20 03:42:44 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Returns string length
+ * @s: Input integer pointer
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	return (len);
+}
