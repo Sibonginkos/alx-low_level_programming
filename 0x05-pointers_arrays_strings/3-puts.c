ba784c86c33366e1021988c3197a190379c25bc4
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-20 04:19:44 -0700

    Prints sting followed by a new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: Pointer to a string
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	_puts(str);
+	_putchar('\n');
+}
