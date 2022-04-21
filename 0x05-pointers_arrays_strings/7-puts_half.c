e847ecf957e2d3e6b0d1f3df596c329a92fb25e4
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-21 03:48:53 -0700

    Half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,17 +10,17 @@ void puts_half(char *str)
 {
 	int i, n;
 
-	for (i = 0, str[i] != 0, i++)
+	for (i = 0; str[i] != 0; i++)
 	{
 	}
 	if (i % 2 == 0)
 	{
-		for ((n = i / 2), str[n] != 0, n++)
+		for ((n = i / 2); str[n] != 0; n++)
 			_putchar(str[n];
 	}
-	else 
+	else
 	{
-		for (n = ((i - 1) / 2), str[n] != 0, n++)
+		for (n = ((i - 1) / 2); str[n] != 0; n++)
 			_putchar(str[n]);
 	}
 	_putchar('\n');
