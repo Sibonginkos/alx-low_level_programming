8ca35ff0cba181ada2e6066af587a539e18661ac
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-23 18:32:36 +0200

    Draw a diagonal shape in terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_diagonal - Draw diagonal line in terminal
- * @n -Input character
+ * print_diagonal - draws a straigt line in terminal
+ * @n: input character
  * Return: Nothing
  */
 
 void print_diagonal(int n)
 {
-	char n;
-	n = "'\'";
+	int i = 0;
 
 	if (n > 0)
-		_putchar("\\n");
-
-	else if(n <= 0)
-		_putchar('\n');
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
 }
