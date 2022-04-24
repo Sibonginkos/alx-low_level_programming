e2c611da468249a168810f1ed6ef486fd7fc8900
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-24 20:18:17 +0200

    main file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -10,3 +15,5 @@ char *leet(char *s);
 char *rot13(char *s);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif /* MAIN_H */
