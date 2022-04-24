838000f51cf970ce6bd73366fcb4cd1f97789571
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-24 19:38:02 +0200

    Print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,25 @@
 
 void print_number(int n)
 {
-	int i;
+	int i = 0;
+	
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_number(n * -1);
+	}
+	else
+		print_number(n);
 
 	if (n / 10 != 0)
 	{
 		_putchar(n % 10);
-		printnumber((n - 1 % 10) / 10);
+		print_number(((n - 1) % 10) / 10);
 	}
 	else if ((n / 10 == 0) && (n / 10 != 0) && (n > 0))
 		_putchar(i % 10);
-	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0)
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
 		_putchar(-n % 10);
 }
