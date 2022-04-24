d7568ec416692c939394c8ea31ebcc4ff91560e8
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-24 18:46:01 +0200

    Encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,6 +11,7 @@ char *rot13(char *s)
 	int i;	
 
 	for (i = 0; s[i] != '\0'; i++)
+	{
 		while ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
 		{
 			if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
@@ -18,5 +19,6 @@ char *rot13(char *s)
 			else
 				s[i] = s[i] - 13;
 		}
+	}
 	return (0);
 }
