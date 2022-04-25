420c8fbe2f60cd108c68238aac156a6cf16621cd
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-25 16:39:09 +0200

    Add two integers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,17 +10,12 @@ char *rot13(char *s)
 {
 	int i = 0;	
 
-	while (s[i] != '\0')
+	for ( ; s[i] != '\0'; i++)
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 				s[i] = s[i] + 13;
 			else
-				s[i] = s[i] - 13;
-			i++;
-		}
-		i++;
+				s[i] = s[i] - 13;	
 	}
 	return (s);
 }
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * infinite add - adds two numbers
+ * @n1: A character pointer
+ * @n2: A character pointer
+ * @r: Buffer to store result
+ * @size_r: Buffer size
+ * Return: A pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i;
+	char *p = n1 + n2;
+
+	for (i = 0; i < size_; i++)
+		return (p);
+	r++;
+	if (p != r)
+		return (0);
+}
