9f528039b25b7785900a08a7f6d4024b959a47fa
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-26 19:23:28 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strchr - Locates a character in a string 
+ * _strchr - Locates a character in a string
  * @s: A character pointer
  * @c: An input integer
  * Return: A pointer to the first occurence of c, else NULL
