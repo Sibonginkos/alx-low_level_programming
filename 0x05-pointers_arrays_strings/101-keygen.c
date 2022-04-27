9ecb08bf9648092dc26a5c6520b65e78d4da8f15
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-04-27 11:56:49 +0200

    Random paswword generation

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,16 +8,61 @@
  * Return: Always 0
  */
 
-int main(void)
+void randomPasswordGeneration(int N)
 {
-	int i;
-	char c[] = "[a-zA-Z] || [0-9]";
-	char ps[];
-	srand((unsigned)time(NULL));
+	int i = 0;
+	
+	int randomizer = 0;
+	
+	srand((unsigned int)(time(NULL)));
+	
+	char numbers[] = "0123456789";
+	
+	char letter[] = "a-z";
+	
+	char LETTER[] = "A-Z";
 
-	for (i = 0; c[i] != '\0'; i++)
-		c[i] = rand() % 58;
-		p = getRandom();
-	printf("%s", c);
+	char symbols[] = "!@#$^&*?";
+	
+	char password[N];
+	
+	randomizer = rand() % 4;
+
+
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = LETTER[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letter[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+	}
+}
+
+int main()
+{
+	int N = 20;
+
+	randomPasswordGeneration(N);
+
 	return (0);
 }
