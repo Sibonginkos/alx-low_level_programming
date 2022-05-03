f659f647b3c9ce333ebae4296f1b0d10c2c74997
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-03 12:58:42 +0200

    A program that adds two positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,18 +14,18 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
+	if (argc < 1)
+		printf("0\n");
+	if (argc >= '0' && argc <= '9')
+	{
+	printf("Error\n");
+		return (1);
+	}
 	for (i = 0; argv[argc][i] != '\0'; i++)
 	{
-		if (argc == 1)
-			printf("0\n");
-		if (argc >= 0 && argc <= 9)
-		{
-			printf("Error\n");
-			return (1);
-		}
-	num = atoi(argv[argc]);
-	sum += num;
-	printf("%d\n", sum);
+		num = atoi(argv[argc]);
+		sum += num;
+		printf("%d\n", sum);
 	}
 	return (0);
 }
