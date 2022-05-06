53d1747273545c718c1c3359150486a6c195e20c
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-06 17:09:13 +0200

    Concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,8 +20,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s = malloc(sizeof(*s1 + *s2));
 	if (s == NULL)
 		return (NULL);
-	if (s1 == NULL || s2 == NULL)
-		return ("");
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; i < n && s2[i] != '\0'; i++)
 		s1[dlen + i] = s2[i];
 	s1[dlen + i] = '\0';
