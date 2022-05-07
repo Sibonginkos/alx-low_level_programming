17aebacff17445457f3b52ecdd436fc588ef16db
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-07 16:00:04 +0200

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,15 +14,15 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf ("Error\n");
-		exit (98);
+		printf("Error\n");
+		exit(98);
 	}
 	else
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		mul = num1 * num2;
-		printf ("%d\n", mul);
+		printf("%d\n", mul);
 		return (0);
 	}
 }
