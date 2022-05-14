1000699a7eef2ea30a332ba8c1ca86761c88c745
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-14 11:20:55 +0200

    Print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil");
 		else
 			printf("%s", s);
-		
 		if (separator == NULL)
 			continue;
 		if (i < (n - 1))
