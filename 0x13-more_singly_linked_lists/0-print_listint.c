82b09725552f8e65c6f4e01cf7691b482333d285
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-24 16:15:46 +0200

    Prints all elements of a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-	unsigned int nodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		printf("[%u] ", h->len);
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("%s\n", h->str);
+		printf("[%d] ", h->n);
 		h = h->next;
 		nodes++;
 	}
