77cf58a34bd8577590fc2051bb790acf927270ff
Sibonginkos <sibonginkosimaseko@gmail.com>
2022-05-24 17:31:55 +0200

    Frees a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,12 +10,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	temp = head;
 	while (head)
 	{
 		temp = head;
 		head = head->next;
-		free(temp->str);
 		free(temp);
 	}
 	free(head);
